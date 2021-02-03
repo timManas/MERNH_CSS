bfff36aaefaf9c59a5202d895ca86f79dadb5dd2
TimManas <timothy.romero.manas@gmail.com>
2021-02-02 19:17:15 -0500

    Add base CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter, Route } from 'react-router-dom'
+import HomeScreen from './components/HomeScreen'
+import LoginScreen from './components/LoginScreen'
+import Header from './screens/Header'
+import Footer from './screens/Footer'
 
 function App() {
   return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
---
+++ frontend/src/screens/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark'>Header</Navbar>
+    </header>
+  )
+}
+
+export default Header
